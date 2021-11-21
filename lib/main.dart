faf125ed1960e2bf7642eb7dc9a1f02d1d245c6c
David Manukov <dava.manukov@yandex.ru>
2021-11-21 18:50:24 +0300

    Test commit

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: 'Flutter Demo',
+        title: 'Gambit',
         debugShowCheckedModeBanner:false,
         home: LoginScreen());
 
