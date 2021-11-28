cd242b174a7aee3fcc51c8afbeaa901090e3aab1
vlad2000121 <vi281412@gmail.com>
2021-11-29 02:17:21 +0300

    change loading

--- lib/pages/welcome_page.dart
+++ lib/pages/welcome_page.dart
@@ -14,7 +14,7 @@ class _WelcomePageState extends State<WelcomePage> {
   @override
   void initState() {
 // Задержка для отображения загрузки, переход на выбранный Pages
-    Future.delayed(Duration(seconds: 3),(){
+    Future.delayed(Duration(seconds: 2),(){
       Navigator.pushReplacement(context, MaterialPageRoute(builder: (context)=>SignupScreen()));
     });
    
@@ -33,14 +33,15 @@ class _WelcomePageState extends State<WelcomePage> {
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
           SizedBox(
-            height: 300,
-            width: 300,
+            height: 500,
+            width: 500,
             child: Lottie.asset('assets/welcome/chess.json')),
-          // SizedBox(height: 10),
+            SizedBox(height: 10),
           Text("Gambit",
               style: TextStyle(
               fontSize: 35,
               fontWeight: FontWeight.bold),
+
           ),
         ],
       ),),
